02ee6964d6c31cd264d8d9c56fc85fc1f8befd0f
Dejanee Gomez <dejaneegomez@gmail.com>
2024-01-20 17:03:27 -0500

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Write your solution in this file!
+ var customerName = 'bob'
+console.log(customerName);
+
+function upperCaseCustomerName() {
+  customerName = 'BOB'
+  return customerName
+}
+
+function setBestCustomer() {
+ bestCustomer = 'not bob'
+return setBestCustomer
+}
+
+function overwriteBestCustomer() {
+     bestCustomer = 'maybe bob'
+    return bestCustomer
+}
+const leastFavoriteCustomer = 'tod'
+function changeLeastFavoriteCustomer() {
+leastFavoriteCustomer = 'mary'
+return leastFavoriteCustomer
+}
+
